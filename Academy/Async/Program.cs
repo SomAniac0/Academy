b3a2228f20fc11c49a6133d9872008296e63fe7d
Soma Mayer <138458243+SomAniac0@users.noreply.github.com>
2023-07-10 18:05:28 +0800

    Add files via upload

---
+++ Academy/Async/Program.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace Async
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var results = BenchmarkRunner.Run<ThreadTasks>();
+            //ThreadTasks test = new ThreadTasks();
+
+
+            //var test2 = await test.GetWebsitesAsync();
+            //var test3 = test.GetWebsitesSync();
+
+
+
+            //var asd = test.GetWebsiteContentParallel3();
+            //var asd2 = test.GetWebsiteContentParallel10();
+
+
+            //foreach (var item in asd) 
+            //{
+            //    Console.WriteLine(item.Length);
+            //};
+
+        }
+    }
+}
